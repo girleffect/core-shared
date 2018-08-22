47276abdcde8d9d17d0387ed0e859839a6f6e9de
David Oberholzer <david.oberholzer@praekeltconsulting.com>
2018-08-22 21:10:35 +0200

    Middleware change

--- ge_core_shared/middleware.py
+++ ge_core_shared/middleware.py
@@ -14,10 +14,14 @@ except ImportError:
     UNPROTECTED_API_ENDPOINTS = set()
 
 
-class AuthMiddleware(object):
+class SharedMiddleware(object):
 
-    def __init__(self, app):
+    def __init__(self, app, prefix):
         self.app = app
+        self.H = Histogram(f"{prefix}_http_duration_seconds", "API duration",
+                           ["path_prefix", "method", "status"])
+        self.app.before_request(self.start_timer)
+        self.app.after_request(self.stop_timer)
 
     def __call__(self, environ, start_response):
         request = Request(environ)
@@ -34,26 +38,6 @@ class AuthMiddleware(object):
         # Deny the API call.
         response = Response("Unauthorized", status="401")
         return response(environ, start_response)
-
-
-class MetricMiddleware(object):
-
-    def __init__(self, app, prefix):
-        self.app = app
-        self.H = Histogram(f"{prefix}_http_duration_seconds", "API duration",
-              ["path_prefix", "method", "status"])
-        self.app.before_request(self.start_timer)
-        self.app.after_request(self.stop_timer)
-
-    # def __call__(self, environ, start_response):
-    #     request = Request(environ)
-    #     start_time = time.time()
-    #     response = self.app(environ, start_response)
-    #     self.H.labels(
-    #         path_prefix=request.path.split("/")[1],
-    #         method=request.method,
-    #         status=response.status).observe(time.time()-start_time)
-    #     return response
 
     @staticmethod
     def start_timer():
