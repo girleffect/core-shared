afeda6698ec5d7c0294c4c9750758f2ebcace439
David Oberholzer <david.oberholzer@praekeltconsulting.com>
2018-08-22 14:28:19 +0200

    Changed decorator to class

--- ge_core_shared/decorators.py
+++ ge_core_shared/decorators.py
@@ -1,64 +1,60 @@
 import functools
 import logging
 
-from types import FunctionType, ModuleType
+from types import FunctionType
 
 from prometheus_client import Histogram
 
 logger = logging.getLogger(__name__)
 
-def _prometheus_module_metric_decorator(f: FunctionType, H: Histogram):
-    """
-    A Prometheus decorator adding timing metrics to a function.
-    This decorator will work on both asynchronous and synchronous functions.
-    Note, however, that this function will turn synchronous functions into
-    asynchronous ones when used as a decorator.
-    :param f: The function for which to capture metrics
-    """
-    module_ = f.__module__.split(".")[-1]
-    call_key = "{}_{}".format(module_, f.__name__)
-    @functools.wraps(f)
-    def wrapper(*args, **kwargs):
-        with H.labels(call=call_key).time():
-            return f(*args, **kwargs)
-    return wrapper
+class MetricsDecorator:
 
+    def __init__(self, module_, service_name, whitelist=None):
+        self.module_ = module_
+        self.H = Histogram(f"{service_name}_call_duration_seconds",
+                           "API call duration (s)", ["call"])
+        self.whitelist = whitelist or []
 
-def add_prometheus_metrics_for_module(module_: ModuleType, service_name: str):
-    """
-    Convenience function applying the Prometheus metrics decorator to the
-    specified module's functions.
-    :param module_: The module to which the instrumentation will be applied
-    :param service_name: The name of the core service.
-    """
-    H = Histogram(f"{service_name}_call_duration_seconds", "API call duration (s)",
-              ["call"])
-    decorate_all_in_module(module_, _prometheus_module_metric_decorator, [], H)
+    def _prometheus_module_metric_decorator(self, f: FunctionType):
+        """
+        A Prometheus decorator adding timing metrics to a function.
+        This decorator will work on both asynchronous and synchronous functions.
+        Note, however, that this function will turn synchronous functions into
+        asynchronous ones when used as a decorator.
+        :param f: The function for which to capture metrics
+        """
+        module_ = f.__module__.split(".")[-1]
+        call_key = "{}_{}".format(module_, f.__name__)
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            with self.H.labels(call=call_key).time():
+                return f(*args, **kwargs)
+        return wrapper
 
-def decorate_all_in_module(module_: ModuleType, decorator: FunctionType, whitelist: list, H: Histogram):
-    """
-    Decorate all functions in a module with the specified decorator
-    :param module_: The module to interrogate
-    :param decorator: The decorator to apply
-    :param whitelist: Functions not to be decorated.
-    :param H: The Prometheus Histogram object.
-    """
-    for name in dir(module_):
-        if name not in whitelist:
-            obj = getattr(module_, name)
-            if isinstance(obj, FunctionType):
-                # We only check functions that are defined in the module we
-                # specified. Some of the functions in the module may have been
-                # imported from other modules. These are ignored.
-                if obj.__module__ == module_.__name__:
-                    logger.debug(f"Adding metrics to {module_}:{name}")
-                    setattr(module_, name, decorator(obj, H))
+    def decorate_all_in_module(self):
+        """
+        Decorate all functions in a module with the specified decorator
+        :param module_: The module to interrogate
+        :param decorator: The decorator to apply
+        :param whitelist: Functions not to be decorated.
+        :param H: The Prometheus Histogram object.
+        """
+        for name in dir(self.module_):
+            if name not in self.whitelist:
+                obj = getattr(self.module_, name)
+                if isinstance(obj, FunctionType):
+                    # We only check functions that are defined in the module we
+                    # specified. Some of the functions in the module may have been
+                    # imported from other modules. These are ignored.
+                    if obj.__module__ == self.module_.__name__:
+                        logger.debug(f"Adding metrics to {self.module_}:{name}")
+                        setattr(self.module_, name, self._prometheus_module_metric_decorator(obj))
+                    else:
+                        logger.debug(f"No metrics on {self.module_}:{name} because it belongs to another "
+                                     f"module")
                 else:
-                    logger.debug(f"No metrics on {module_}:{name} because it belongs to another "
-                                 f"module")
-            else:
-                logger.debug(f"No metrics on {module_}:{name} because it is not a coroutine or "
-                             f"function")
+                    logger.debug(f"No metrics on {self.module_}:{name} because it is not a coroutine or "
+                                 f"function")
 
 def list_response(func):
     """
