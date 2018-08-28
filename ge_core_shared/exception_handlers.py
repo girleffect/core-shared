35dea288315b46d1eb393b41cdc9151bbdee918f
AltusBarry <AltusBarry@users.noreply.github.com>
2018-08-28 12:59:15 +0200

    feature/GEINFRA-256: Lookup psycopg error codes and change http status code if needed

--- ge_core_shared/exception_handlers.py
+++ ge_core_shared/exception_handlers.py
@@ -1,12 +1,22 @@
 import logging
 import json
 
+from psycopg2 import errorcodes
+
 from project.app import DB as db
 
 logger = logging.getLogger(__name__)
 
+PG_ERROR_STATUS_CODE_MAP = {
+    "UNIQUE_VIOLATION": 409
+}
 
 def db_exceptions(exception):
     logger.error(exception)
     db.session.rollback()
-    return json.dumps({"error": exception._message().replace("\n", " ")}), 500
+    error_type = errorcodes.lookup(exception.orig.pgcode)
+    return json.dumps(
+        {
+            "error": exception.orig.pgerror
+        }
+    ), PG_ERROR_STATUS_CODE_MAP.get(error_type, 500)
