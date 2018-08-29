3c0e2870afec0fef379bde896a1811d2c38ca6b4
AltusBarry <AltusBarry@users.noreply.github.com>
2018-08-29 10:06:10 +0200

    feature/GEINFRA-256: Alter mapping logic a bit

--- ge_core_shared/exception_handlers.py
+++ ge_core_shared/exception_handlers.py
@@ -8,15 +8,35 @@ from project.app import DB as db
 logger = logging.getLogger(__name__)
 
 PG_ERROR_STATUS_CODE_MAP = {
-    "UNIQUE_VIOLATION": 409
+    # Entire postgres error class to set the status for
+    "pgclass_23": 400,
+
+    # Specific errors to set the status for
+    "UNIQUE_VIOLATION": 409,
 }
 
 def db_exceptions(exception):
     logger.error(exception)
     db.session.rollback()
     error_code = errorcodes.lookup(exception.orig.pgcode)
+
+    # Postgres errors are split into different classes, the class is obtainable
+    # from the first 2 characters in the postgres error code. This is useful if
+    # there is a need to set a status code for an entire class rather than
+    # specific errors. Classes can be found here:
+    # https://www.postgresql.org/docs/current/static/errcodes-appendix.html#ERRCODES-TABLE
+    error_class = f"pgclass_{exception.orig.pgcode[:2]}"
+
+    # Set the status code, check entire class first then see if specific error
+    # has a status code mapped.
+    http_status_code = PG_ERROR_STATUS_CODE_MAP.get(
+        error_class, 500
+    )
+    http_status_code = PG_ERROR_STATUS_CODE_MAP.get(
+        error_code, http_status_code
+    )
     return json.dumps(
         {
             "error": exception.orig.pgerror.strip().replace("\n", " ")
         }
-    ), PG_ERROR_STATUS_CODE_MAP.get(error_code, 500)
+    ), http_status_code
